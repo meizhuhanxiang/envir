008d8a735e28cf7f681e7dac7a2d8b90cff5a866
Zeng Yuan <zengyuan@haizhi.com>
2015-03-29 15:40:33 +0800

    install bug

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 #clean environment
-rm -rf "$HOME/.bash*"
-rm -rf "$HOME/.vim*"
+rm -rf "$HOME"/.bash*
+rm -rf "$HOME"/.vim*
 
-####  Bash
+###  Bash
 path=`pwd .`
 for file in bash/*; do
   basefile=`basename $file`
   #echo "$HOME/.$file"
-  ln -sf "$path/$file" "$HOME/.$basefile"
+  ln -sf "$path"/"$file" "$HOME"/."$basefile"
 done
 
 #### Vim
-ln -sf "$path/vimrc" "$HOME/.vimrc"
-ln -sf "$path/vim" "$HOME/.vim"
+ln -sf "$path"/vimrc "$HOME"/.vimrc
+ln -sf "$path"/vim "$HOME"/.vim
 
 #### Git
-ln -sf "$path/gitignoreglobal" "$HOME/.gitignoreglobal"
+ln -sf "$path"/gitignoreglobal "$HOME"/.gitignoreglobal
 
 bash
